e95f8253da177e6a969b7e3530de84db1ecee4e9
unknown <nikitap@PCBKK-D6G3XY42.crp.unops.local>
2015-05-26 14:58:32 +0700

    Feature: example of repository and manager classes.

---
+++ Kanbang.Core/DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace Kanbang.Core.DataAccess.Repositories
+{
+	using System.Collections.Generic;
+	using System.Data;
+	using System.Linq;
+	using Dapper;
+	using Kanbang.Core.Domain;
+
+	internal static class UserRepository
+	{
+		public static void ChangeUserName(this IDbConnection connection, int userId, string name)
+		{
+			connection.Execute("update [User] set Name = @Name where Id = @Id", new { Name = name, Id = userId });
+		}
+
+		public static User Get(this IDbConnection connection, int userId)
+		{
+			return connection.Query<User>("select * from [User] where Id = @Id", userId).SingleOrDefault();
+		}
+
+		public static IEnumerable<User> GetUsers(this IDbConnection connection)
+		{
+			return connection.Query<User>("select * from [User]");
+		}
+	}
+}
---
+++ Kanbang.Core/Managers/UserManager.cs
@@ -0,0 +1,25 @@
+﻿namespace Kanbang.Core.Managers
+{
+	using System.Data;
+	using Kanbang.Core.DataAccess.Repositories;
+
+	public class UserManager
+	{
+		private readonly IDbConnection connection;
+
+		public UserManager(DataContext dataContext)
+		{
+			this.connection = dataContext.DbConnection;
+		}
+
+		public void ChangeNamesOfAllUsers(string name)
+		{
+			var users = this.connection.GetUsers();
+
+			foreach (var user in users)
+			{
+				this.connection.ChangeUserName(user.Id, name);
+			}
+		}
+	}
+}
