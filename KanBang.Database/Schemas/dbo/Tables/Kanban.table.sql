f5942adc94b2f34702604136c6a18d0cc9fee6ce
niaher <niaher.crimston@gmail.com>
2015-06-29 23:51:49 +0700

    Refactor: removed kanban start and end dates.

--- KanBang.Database/Schemas/dbo/Tables/Kanban.table.sql
+++ KanBang.Database/Schemas/dbo/Tables/Kanban.table.sql
@@ -4,8 +4,6 @@
 	[Title] nvarchar(250) NOT NULL,
 	[Description] nvarchar(MAX) NULL,	
 	[ProjectId] int NOT NULL,
-	[DateStart] datetime NULL,
-	[DateEnd] datetime NULL,
 	[CreatedOn] datetime NOT NULL,
 	[CreatedBy] int null
 )
